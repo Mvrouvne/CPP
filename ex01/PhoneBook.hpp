7b85c4c01efe4306c6058503853c5a173273c49e
Marouane Chaibat <machaiba@192.168.1.7>
2023-09-28 00:37:03 +0100

    cpp

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -7,6 +7,13 @@ class   PhoneBook
 {
     private:
         std::string contacts[8];
+        int         index;
+        std::string **arr;
+    public:
+        void    save_phonebook(std::string first_name, std::string last_name,
+									std::string nickname, std::string darkest_secret,
+									int phone_number);
+        void    search();
 };
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,9 +1,53 @@
 #include "PhoneBook.hpp"
 #include "Contacts.hpp"
 
+void	PhoneBook::search()
+{
+	std::cout << "Heeeree" << std::endl;
+	printf("[%s]\n", arr[0][0]);
+	std::cout << arr[0] << std::endl;
+}
+
+
+void	PhoneBook::save_phonebook(std::string first_name, std::string last_name,
+									std::string nickname, std::string darkest_secret,
+									int phone_number)
+{
+	static int	n;
+	Contacts	obj;
+	int			x;
+	int			y;
+	int			i;
+	// std::string		**arr;
+
+	// if (n == 0)
+		arr = new std::string*[8];
+	// contacts[n] = save_phonebook(first_name, last_name, nickname,
+	// 						darkest_secret, phone_number);
+
+	i = 0;
+	while (i < 8)
+	{
+		arr[i] = new std::string[5];
+		i++;
+	}
+	x = 0;
+	if (n < 8)
+	{
+		arr[n][0] = first_name;
+		arr[n][1] = last_name;
+		arr[n][2] = nickname;
+		arr[n][3] = darkest_secret;
+		arr[n][4] = phone_number;
+		index = n;
+		n++;
+	}
+	std::cout << n << std::endl;
+}
 
 void	Contacts::save_contacts(void)
 {
+	PhoneBook obj;
 	// while (1337)
 	// {
 		std::cout << "Enter first name: " << std::endl;
@@ -17,11 +61,14 @@ void	Contacts::save_contacts(void)
 		std::cout << "Enter darkest secret: " << std::endl;
 		std::cin >> darkest_secret;
 	// }
+	obj.save_phonebook(first_name, last_name, nickname,
+							darkest_secret, phone_number);
 }
 
 int	main()
 {
 	Contacts	contacts;
+	PhoneBook	phonebook;
 	std::string command;
 
 	while (1337)
@@ -29,9 +76,12 @@ int	main()
 		std::cout << "Enter a command: " << std::endl;
 		std::cin >> command;
 		if (command == "ADD")
+		{
 			contacts.save_contacts();
+			// std::cout <<  << std::endl;
+		}
 		if (command == "EXIT")
 			break ;
-
+		phonebook.search();
 	}
 }
