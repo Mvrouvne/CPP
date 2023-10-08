44f03077cb231a4e59de023a60c35c247a5f5a41
Marouane Chaibat <machaiba@Mr1s-Macbook-Pro.local>
2023-10-08 18:51:25 +0100

    ex05

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -1,1 +1,44 @@
 #include "Harl.hpp"
+
+void	Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void	Harl::waring(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	Harl	obj;
+	std::string	arr[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int	x = 0;
+	void	(Harl::*ptr_to_member[4])() = {
+		&Harl::debug,
+		&Harl::info,
+		&Harl::waring,
+		&Harl::error
+	};
+	while(x < 4)
+	{
+		if (level == arr[x])
+		{
+			(obj.*ptr_to_member[x])();
+			break ;
+		}
+		x++;
+	}
+
+}
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -1,2 +1,8 @@
 #include "Harl.hpp"
 
+int	main()
+{
+	Harl	obj;
+
+	obj.complain("WARNING");
+}
