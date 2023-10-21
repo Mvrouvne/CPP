3b63940468db7d1bed09707f087424abf543ea5e
Marouane Chaibat <machaiba@e1r7p4.1337.ma>
2023-10-21 13:54:20 +0100

    cpp02 finished

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -2,20 +2,15 @@
 
 int	main( void )
 {
-	// Fixed a;
-	// Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	// std::cout << a << std::endl;
-	// std::cout << ++a << std::endl;
-	// std::cout << a << std::endl;
-	// std::cout << a++ << std::endl;
-	// std::cout << a << std::endl;
-	// std::cout << b << std::endl;
-	// std::cout << Fixed::max( a, b ) << std::endl;
-	// return 0;
-	Fixed a(3);
-	Fixed b(2);
-
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
-	// a++;
-	// std::cout << a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	return 0;
 }
+ 
