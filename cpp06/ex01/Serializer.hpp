1a5c2a6c922346c90322f0f0adcb44c92a9ea910
Marouane Chaibat <machaiba@Mr1s-Macbook-Pro.local>
2023-12-03 09:42:07 +0100

    lkj

---
+++ cpp06/ex01/Serializer.hpp
@@ -0,0 +1,21 @@
+#ifndef SERIALIZER_HPP
+#define SERIALIZER_HPP
+
+#include <iostream>
+// #include <stdint.h>
+
+class   Serializer
+{
+	public:
+		static uintptr_t	serialize(Data* ptr);
+		static Data*		deserialize(uintptr_t raw);
+};
+
+struct	Data
+{
+	int		int_data;
+	char	char_data;
+	double	double_data;
+};
+
+#endif
---
+++ cpp06/ex01/main.cpp
@@ -0,0 +1,9 @@
+// #include "Serializer.hpp"
+#include <iostream>
+
+int	main()
+{
+	int	num1 = 42;
+	double num2 = num1;
+	std::cout << num2 << std::endl;
+}
