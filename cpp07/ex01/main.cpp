17e545a4bf31fc479b632ac2a92ce22098b756a8
Marouane Chaibat <machaiba@Mr1s-MacBook-Air.local>
2023-12-13 15:27:02 +0100

    cpp07 finished

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,9 +1,19 @@
 #include "iter.hpp"
 
+template<typename T>
+void print(T& a)
+{
+		std::cout << a << std::endl;
+}
+
 int	main()
 {
-	std::string arr[3] = {"Hello", "Hey", "Hi"};
-	iter(arr, 3, arr_function<std::string>);
+	// std::string arr[3] = {"Hello", "Hey", "Hi"};
+	// iter(arr, 3, print<std::string>);
+	// std::string arr[3] = {"Hello", "Hey", "Hi"};
+	// iter(arr, 3, arr_function<std::string>);
 	// int arr[3] = {3, 2, 1};
 	// iter(arr, 3, arr_fun<int>);
+	int arr[3] = {3, 2, 1};
+	iter(arr, 3, print<const int>);
 }
