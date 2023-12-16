64bfaf19433991fb1bb04c388758f0248ab791e3
Marouane Chaibat <machaiba@Mr1s-MacBook-Air.local>
2023-12-16 17:02:15 +0100

    cpp08 finished

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -7,37 +7,20 @@
 template<typename T>
 class   MutantStack : public std::stack<T>
 {
-    protected:
-    public:
-        T*   elements[]:
+	public:
+		// T*   elements[]:
 		// MutantStack();
 		// MutantStack(const MutantStack& old_obj);
 		// MutantStack&	operator=(const MutantStack& old_obj);
 		// ~MutantStack();
-        class   iterator
-        {
-            iterator(T* ptr)
-            {
-                elements = ptr;
-            }
-            iterator&   operator++()
-            {
-                elements++;
-                retrun *this:
-            }
-            iterator&   operator--()
-            {
-                elements--;
-                return *this;
-            }
-            iterator&   begin()
-            {
-                return  iterator(&elements[0]);
-            }
-            iterator&   end()
-            {
-                return iterators(&elements + size);
-            }
-        };
+		typedef typename std::deque<T>::iterator iterator; // std::deque<T>::iterator is an independent name that's why we did typename
+		iterator begin()
+		{
+			return this->c.begin();
+		}
+		iterator end()
+		{
+			return this->c.end();
+		}
 };
 #endif
